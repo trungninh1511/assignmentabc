061ce016ecb77de0cd1344689fbb1c3bb2505b6d
trungninh1511 <s3574912@rmit.edu.vn>
2019-10-23 22:53:36 +0700

    update_files

--- lecturer.php
+++ lecturer.php
@@ -5,8 +5,9 @@ echo '
 Id: <input type="text" name="id" id="id"><br/>
 FirstName: <input type="text" firstname="id" id="firstname"><br/>
 LastName: <input type="text" lastname="id" id="lastname"><br/>
-Male<input type="radio" name="sex" value="M" /> Male <br />
-Female<input type="radio" name="sex" value="F" /> Female
+Gender:
+<input type="radio" name="sex" value="M" /> Male <br />
+<input type="radio" name="sex" value="F" /> Female
 
 <input type="submit" value="Save">
 </form>
---
+++ lecturerAction.php
@@ -0,0 +1,9 @@
+<?php
+ 
+echo $_POST['id'];
+echo $_POST['name'];
+echo $_POST['firstname'];
+echo $_POST['lastname'];
+echo $_POST['gender'];
+ 
+?>
