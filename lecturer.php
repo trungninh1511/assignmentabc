7bf7847cb6d3b533b2569608816d81bb43dfdabd
trungninh1511 <s3574912@rmit.edu.vn>
2019-10-23 20:22:33 +0700

    lecturer

---
+++ lecturer.php
@@ -0,0 +1,15 @@
+<?php
+
+echo '
+<form action="/customerAction" method="post">
+Id: <input type="text" name="id" id="id"><br/>
+FirstName: <input type="text" firstname="id" id="firstname"><br/>
+LastName: <input type="text" lastname="id" id="lastname"><br/>
+Male<input type="radio" name="sex" value="M" /> Male <br />
+Female<input type="radio" name="sex" value="F" /> Female
+
+<input type="submit" value="Save">
+</form>
+';
+
+?>
